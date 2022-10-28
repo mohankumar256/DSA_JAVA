40f1435f16f06e25d6e55bcc512a5d03cdbf9dfb
Mohan Kumar <mohanvbb9899@gmail.com>
2022-10-28 18:50:52 +0530

    pattern 3

--- Patterns/pattern2.java
+++ Patterns/pattern2.java
@@ -1,14 +1,14 @@
 import java.util.Scanner;
 
-public class pattern1 {
+public class pattern2 {
 
 	public static void main(String[] args) {
 		Scanner scn = new Scanner(System.in);
 		int n = scn.nextInt();
 
-		for (int i = 1; i <= n; i++) { // for rows
-			
-			for (int j = 1; j <= i; j++) // for stars
+		for (int i = n; i <= 1; i--) { // for rows
+
+			for (int j = i; j <= 1; j--) // for stars
 				System.out.print("*\t");
 
 			System.out.println();
